b17657d824ecdfb9357bcf04dc9283058d14dacd
HumoFX <humo_otahanov@icloud.com>
2022-05-20 01:51:56 +0500

    example of class based view for huffman template

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ConditionForm
 
@@ -11,8 +12,8 @@ def encode(request):
     return render(request, "encode.html")
 
 
-def huffman(request):
-    return render(request, "huffman.html")
+# def huffman(request):
+#     return render(request, "huffman.html")
 
 
 def hamming(request):
@@ -36,3 +37,33 @@ def hamming(request):
 #         form = ConditionForm()
 #
 #     return render(request, "huffman.html", {'form': form})
+
+def to_list(cond: str) -> list:
+    numbers = cond.split(' ')
+    map_cond = map(int, numbers)
+    numbers = list(map_cond)
+    return numbers
+
+
+def do_sum(cond: str):
+    numbers = to_list(cond)
+    result = sum(numbers)
+    return result
+
+
+class HuffmanView(generic.FormView):
+    template_name = 'huffman.html'
+    form_class = ConditionForm
+    success_url = '/huffman/'
+    plus_context = dict()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['plus_context'] = self.plus_context
+        return context
+
+    def form_valid(self, form):
+        condition = form.data.get('condition')
+        if condition:
+            self.plus_context['result'] = do_sum(condition)
+        return super().form_valid(form)
